ce9d8c06b5fe70b5af46a29df7d5a06cd57cef9d
Krzysztof <krzysztof.b.bielecki@gmail.com>
2024-01-23 16:50:39 +0100

    komponent produktów

---
+++ src/app/modules/product/model/product.ts
@@ -0,0 +1,7 @@
+export interface Product{
+    name: string,
+        category: string,
+        desription: string,
+        price: number,
+        currency: string
+}
--- src/app/modules/product/product.component.ts
+++ src/app/modules/product/product.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ProductService } from './product.service';
+import { Product } from './model/product';
 
 @Component({
   selector: 'app-product',
   templateUrl: './product.component.html',
   styleUrl: './product.component.scss'
 })
-export class ProductComponent {
+export class ProductComponent implements OnInit {
+
+  constructor(private productService: ProductService) {}
+
+  products: Product[] = [];
+
+  ngOnInit(): void {
+    this.getProducts();
+  }
+
+  getProducts() {
+    this.products = this.productService.getProducts();
+  }
 
 }
