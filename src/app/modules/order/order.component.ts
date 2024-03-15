0d98bb939d6571badc42d7c0060018dd1dd1994a
Krzysztof <krzysztof.b.bielecki@gmail.com>
2024-03-15 18:52:43 +0100

    refactoring

--- src/app/modules/order/order.component.ts
+++ src/app/modules/order/order.component.ts
@@ -18,6 +18,7 @@ export class OrderComponent implements OnInit {
   formGroup!: FormGroup;
   orderSummary!: OrderSummary;
   initData!: InitData;
+  errorMessage = false;
 
   private statuses = new Map<string, string>([
     ["NEW","Nowe"]
@@ -66,10 +67,14 @@ export class OrderComponent implements OnInit {
         shipmentId: Number(this.formGroup.get('shipment')?.value.id),
         paymentId: Number(this.formGroup.get('payment')?.value.id)
       } as OrderDto)
-        .subscribe(orderSummary => {
-          this.orderSummary = orderSummary;
-          this.cookieService.delete("cartId");
-        })
+        .subscribe({
+          next: orderSummary => {
+            this.orderSummary = orderSummary;
+            this.cookieService.delete("cartId");
+            this.errorMessage = false;
+        },
+        error: err => this.errorMessage = true
+      })
     }
   }
 
